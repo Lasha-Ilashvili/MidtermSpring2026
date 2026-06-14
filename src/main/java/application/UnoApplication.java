281dd2a525fb1c61e00ad418acf90b09c31e551e
Lasha-Ilashvili <ilashvililasha@gmail.com>
2026-06-15 03:51:08 +0400

    Close CLI application resources

--- src/main/java/application/UnoApplication.java
+++ src/main/java/application/UnoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
@@ -20,7 +21,9 @@ import ui.UiType;
 public class UnoApplication {
 
     public static void run(String[] args) {
-        SpringApplication.run(UnoApplication.class, args);
+        try (ConfigurableApplicationContext ignored =
+                     SpringApplication.run(UnoApplication.class, args)) {
+        }
     }
 
     @Bean
